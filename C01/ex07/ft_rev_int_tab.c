ae2f7d5aec35abc51f75c9e40d55976fb04239f6
Rayanne Murras <72751669+RayMurras@users.noreply.github.com>
2022-07-25 17:24:53 +0100

    Update ft_rev_int_tab.c

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	i;	
+	int	i;
 	int	swap;
-
 	i = 0;
+	
 	while (i < (size / 2))
 	{
 		swap = tab[i];
@@ -15,12 +15,13 @@ void	ft_rev_int_tab(int *tab, int size)
 	}
 }
 
-int	main()
+int main()
 {
-	int tab[6] = {0, 1, 2, 3, 4, 5};
-	int size = 6;
+    int tab[6] = {0, 4, 5, 6, 7, 8};
+    int size = 6;
 
-	ft_rev_int_tab(tab, size);
-	printf("%i, %i, %i, %i, %i, %i", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]); 
-	return (0);
-}    
+    ft_rev_int_tab(tab, size);
+    printf("%i, %i, %i, %i, %i, %i", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]);
+    return 0;
+}
+    
